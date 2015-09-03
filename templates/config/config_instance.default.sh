78e153f3fd7b4584323e850a6371422aa3b3eeaf
Ratcash Developer <developer@ratcash.net>
2015-09-03 10:30:37 +0200

    Allow Ansible to configure the instances after deployment

---
+++ templates/config/config_instance.default.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# $1 - The first argument is always the directory where asadmin resides.
+# $2 - The second argument to the script is the gf_env_instance_node variable
+
+$1/asadmin start-domain domain1
+
+# Enable protocol and user mapping from the Reverse-Proxy
+$1/asadmin set configs.config.server-config.network-config.protocols.protocol.http-listener-1.http.scheme-mapping=X-Forwarded-Proto
+$1/asadmin set configs.config.server-config.network-config.protocols.protocol.http-listener-1.http.remote-user-mapping=X-Forwarded-User
